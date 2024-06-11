f8ad1b96097e6f2635fdd35bfeae09952fe491bb
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-10 22:15:03 -0300

    style: adjusted style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="contained">Cadastrar</Button>
+      <div className='inputContainer'>
+        <TextField id="standard-basic" label="User" variant="standard" />
+        <ButtonGroup variant="contained" aria-label="Basic button group">
+          <Button>Gravar</Button>
+          <Button>Alterar</Button>
+          <Button>Deletar</Button>
+        </ButtonGroup>
+      </div>
     </div>
   );
 }
