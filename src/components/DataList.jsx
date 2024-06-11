2c8b89c080a4b76880d07e3774d731bf738553d8
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-10 22:19:20 -0300

    created new component DataList

---
+++ src/components/DataList.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function DataList(){
+    const [ data, setData ] = useState('');
+
+    return(
+        <div className="listas">
+            
+        </div>
+    );
+}
+
+export default DataList;
