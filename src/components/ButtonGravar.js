b57acd6b149f4046099a5304b27cdef82e62b105
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-13 22:25:10 -0300

    fix: save utilizando resquestbody

--- src/components/ButtonGravar.js
+++ src/components/ButtonGravar.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 function ButtonGravar() {
 
-    function gravar(){
-        axios.get('http://localhost:8080/pastelaria/lista')
+    function gravar(sabor){
+        axios.get('http://localhost:8080/pastelaria/inserir', { sabor : sabor} )
           .then(function (resposta) {
             console.log('gravado com sucesso' + resposta.data)
           })
@@ -18,3 +18,40 @@ function ButtonGravar() {
   }
 
 export default ButtonGravar;
+
+
+// import React, { useState } from 'react';
+
+// function MeuComponente() {
+//     const [sabor, setSabor] = useState('');
+
+//     function handleInputChange(event) {
+//         setSabor(event.target.value);
+//     }
+
+//     function handleButtonClick() {
+//         enviarSaborDoPastel(sabor);
+//     }
+
+//     return (
+//         <div>
+//             <input type="text" value={sabor} onChange={handleInputChange} />
+//             <button onClick={handleButtonClick}>Inserir Pastel</button>
+//         </div>
+//     );
+// }
+
+// export default MeuComponente;
+
+// import axios from 'axios';
+
+// function enviarSaborDoPastel(sabor) {
+//     axios.post('http://localhost:8080/inserir', { sabor: sabor })
+//          .then(response => {
+//              console.log('Pastel inserido com sucesso:', response.data);
+//              // Faça o que for necessário com a resposta, como atualizar a lista de itens
+//          })
+//          .catch(error => {
+//              console.error('Erro ao inserir pastel:', error);
+//          });
+// }
